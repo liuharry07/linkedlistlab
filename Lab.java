56f8d817f469c4759e196b64d56822f47eb72d7a
liuharry07 <liuharry07@gmail.com>
2022-08-31 12:19:16 -0400

    8/31/2022

--- Lab.java
+++ Lab.java
@@ -1,4 +1,4 @@
-//For Additional Credit Tester - not used in exercises
+// For Additional Credit Tester - not used in exercises
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
@@ -10,7 +10,7 @@ public class Lab {
 
     public static int size(ListNode<String> list) {
         int total = 0;
-        while (list != null) {
+        while(list != null) {
             list = list.getNext();
             ++total;
         }
@@ -19,8 +19,8 @@ public class Lab {
 
     public static boolean contains(ListNode<String> list, String str) {
         boolean x = false;
-        while (list != null) {
-            if (list.getValue().equals(str)) {
+        while(list != null) {
+            if(list.getValue().equals(str)) {
                 x = true;
             }
             list = list.getNext();
@@ -29,21 +29,20 @@ public class Lab {
     }
 
     public static boolean add(ListNode<String> list, String str) {
-        if (!contains(list, str)) {
-            while (list.getNext() != null) {
+        if(!contains(list, str)) {
+            while(list.getNext() != null) {
                 list = list.getNext();
             }
             list.setNext(new ListNode<String>(str, null));
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
     public static boolean remove(ListNode<String> list, String str) {
-        if (contains(list, str)) {
-            while (!list.getNext().getValue().equals(str)) {
+        if(contains(list, str)) {
+            while(!list.getNext().getValue().equals(str)) {
                 list = list.getNext();
             }
             list.setNext(list.getNext().getNext());
@@ -55,7 +54,7 @@ public class Lab {
 
     public static ListNode<String> toList(String[] array) {
         ListNode<String> result = null;
-        for (int i = array.length - 1; i >= 0; --i) {
+        for(int i = array.length - 1; i >= 0; --i) {
             ListNode<String> temp = new ListNode<String>(array[i], result);
             result = temp;
         }
@@ -66,15 +65,15 @@ public class Lab {
     public static <E> boolean hasDuplicate(ListNode<E> list) {
         int size = 0;
         ListNode<E> copy = list;
-        while (copy != null) {
+        while(copy != null) {
             copy = copy.getNext();
             ++size;
         }
-        for (int i = 0; i < size; ++i) {
+        for(int i = 0; i < size; ++i) {
             copy = list;
-            for (int j = i; j < size - 1; ++j) {
+            for(int j = i; j < size - 1; ++j) {
                 copy = copy.getNext();
-                if (copy.getValue() == list.getValue()) {
+                if(copy.getValue() == list.getValue()) {
                     return true;
                 }
             }
@@ -84,28 +83,24 @@ public class Lab {
     }
 
     public static void testHasDuplicate() {
-        String[][] tests = { {}, { "A" }, { "A", "A" }, { "A", "B" },
-                { "A", "B", "C", "D", "E" },
-                { "A", "B", "C", "D", "C" },
-                { "A", "A", "C", "D", "E" } };
+        String[][] tests = {{}, {"A"}, {"A", "A"}, {"A", "B"}, {"A", "B", "C", "D", "E"},
+                {"A", "B", "C", "D", "C"}, {"A", "A", "C", "D", "E"}};
         // Expected Results: false, false, true, false, false, true, true
-        for (String[] i : tests) {
+        for(String[] i : tests) {
             System.out.print(Arrays.toString(i));
             System.out.println(" - " + hasDuplicate(toList(i)));
         }
     }
 
     public static ListNode<Integer> insert(ListNode<Integer> list, int value) {
-        if (list.getValue() < value && list.getNext() != null) {
+        if(list.getValue() < value && list.getNext() != null) {
             insert(list.getNext(), value);
-        }
-        else {
+        } else {
             if(list.getValue() >= value) {
                 int temp = list.getValue();
                 list.setValue(value);
                 list.setNext(new ListNode<Integer>(temp, list.getNext()));
-            }
-            else {
+            } else {
                 list.setNext(new ListNode<Integer>(value, list.getNext()));
             }
         }
@@ -113,20 +108,18 @@ public class Lab {
     }
 
     public static void testInsert() {
-        int[][] tests = { { 1, 2, 3, 4, 5 }, 
-            { 1, 1, 5, 5, 9, 9 },
-            { 9, 10, 11, 12, 13 },
-            { 1, 1, 7, 7, 8, 8, 9, 9 } };
-        /* Expected Results:
-            [ 1 | ] -> [ 2 | ] -> [ 3 | ] -> [ 4 | ] -> [ 5 | ] -> [ 8 | null ]
-            [ 1 | ] -> [ 1 | ] -> [ 5 | ] -> [ 5 | ] -> [ 8 | ] -> [ 9 | ] -> [ 9 | null ]
-            [ 8 | ] -> [ 9 | ] -> [ 10 | ] -> [ 11 | ] -> [ 12 | ] -> [ 13 | null ]
-            [ 1 | ] -> [ 1 | ] -> [ 7 | ] -> [ 7 | ] -> [ 8 | ] -> [ 8 | ] -> [ 8 | ] -> [ 9 | ] -> [ 9 | null ]
-        */
+        int[][] tests = {{1, 2, 3, 4, 5}, {1, 1, 5, 5, 9, 9}, {9, 10, 11, 12, 13},
+                {1, 1, 7, 7, 8, 8, 9, 9}};
+        /*
+         * Expected Results: [ 1 | ] -> [ 2 | ] -> [ 3 | ] -> [ 4 | ] -> [ 5 | ] -> [ 8 | null ] [ 1
+         * | ] -> [ 1 | ] -> [ 5 | ] -> [ 5 | ] -> [ 8 | ] -> [ 9 | ] -> [ 9 | null ] [ 8 | ] -> [ 9
+         * | ] -> [ 10 | ] -> [ 11 | ] -> [ 12 | ] -> [ 13 | null ] [ 1 | ] -> [ 1 | ] -> [ 7 | ] ->
+         * [ 7 | ] -> [ 8 | ] -> [ 8 | ] -> [ 8 | ] -> [ 9 | ] -> [ 9 | null ]
+         */
         int value = 8;
-        for (int[] i : tests) {
+        for(int[] i : tests) {
             ListNode<Integer> result = null;
-            for (int j = i.length - 1; j >= 0; --j) {
+            for(int j = i.length - 1; j >= 0; --j) {
                 ListNode<Integer> temp = new ListNode<Integer>(i[j], result);
                 result = temp;
             }
@@ -143,10 +136,10 @@ public class Lab {
             ++size;
         }
         one = list;
-        for (int i = 0; i < size - 1; ++i) {
+        for(int i = 0; i < size - 1; ++i) {
             two = one.getNext();
-            while (two != null) {
-                if (two.getValue() < one.getValue()) {
+            while(two != null) {
+                if(two.getValue() < one.getValue()) {
                     int temp = one.getValue();
                     one.setValue(two.getValue());
                     one.getNext().setValue(temp);
@@ -160,18 +153,24 @@ public class Lab {
     }
 
     public static void testSort() {
-        int[][] tests = {{2, 1, 5, 4, 3}, 
-                        {1, 2, 3, 4, 5}, 
-                        {6, 3, 7, 3, 4, 3, 1}};
+        int[][] tests = {{2, 1, 5, 4, 3}, {1, 2, 3, 4, 5}, {6, 3, 7, 3, 4, 3, 1}};
         for(int[] i : tests) {
             ListNode<Integer> result = null;
-            for (int j = i.length - 1; j >= 0; --j) {
+            for(int j = i.length - 1; j >= 0; --j) {
                 ListNode<Integer> temp = new ListNode<Integer>(i[j], result);
                 result = temp;
             }
             printListNode(sort(result));
         }
-            
+
+    }
+
+    public static <E> ListNode<E> reverse(ListNode<E> list) {
+
+    }
+
+    public static void testReverse() {
+        int[][] tests = {{1, 2, 3, 4, 5}};
     }
 
     public static <E> void printListNode(ListNode<E> list) {
@@ -203,7 +202,7 @@ public class Lab {
         frame.setVisible(true);
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                switch (list.getSelectedIndex()) {
+                switch(list.getSelectedIndex()) {
                     case 0:
                         testHasDuplicate();
                         break;
